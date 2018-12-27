db22a801f972993a9bf82ba767a7047f5337a583
root <dominik.obuch@wp.pl>
2018-12-27 23:55:18 +0100

    added header

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,9 @@
 let test = () => {
     console.log("test")
 }
+let hamburgerBarEl = document.getElementsByClassName("hamburger__bar");
+for(let i=0;i<hamburgerBarEl.length; i++){
+    hamburgerBarEl[i].addEventListener("click",function(){
+        document.getElementById("header-list").classList.toggle("disp-none")
+    })
+}
