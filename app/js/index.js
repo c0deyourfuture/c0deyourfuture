2e4ed5d10276d2a727bea1f1c6d2a9216549116c
root <dominik.obuch@wp.pl>
2018-12-29 22:27:14 +0100

    hamburger animation added

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,8 @@
-let test = () => {
-    console.log("test")
-}
 let hamburgerBarEl = document.getElementsByClassName("hamburger__bar");
 for(let i=0;i<hamburgerBarEl.length; i++){
     hamburgerBarEl[i].addEventListener("click",function(){
-        document.getElementById("header-list").classList.toggle("disp-none")
+        document.getElementById("header-list").classList.toggle("hamburger-animation-close");
+        document.getElementById("header-list").classList.remove("display-none");
+        document.getElementById("header-list").classList.toggle("hamburger-animation-open");
     })
 }
